cf42054091992204c366505b81ad6ba6e32c7ec2
misbba <misbbahoonnishaa@gmail.com>
2026-07-20 19:30:55 +0400

    Fixed image loading

--- src/main/java/com/foodbridge/foodbridge/config/WebConfig.java
+++ src/main/java/com/foodbridge/foodbridge/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
-    }
 
+    }
 }
